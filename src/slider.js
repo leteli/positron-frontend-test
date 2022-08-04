33eecef2549e99a6dae3d93dc433b363b5126c72
Nastia Kartseva <danetzachem@gmail.com>
2022-08-04 18:22:56 +0300

    remove unnecessary file

--- src/slider.js
+++
@@ -1,6 +0,0 @@
-const slider = (items, itemsByPage, pageNumber) => items.filter(
-    (item, index) =>
-      itemsByPage * (pageNumber - 1) <= index < itemsByPage * pageNumber
-  );
-
-export default slider;
