6ca990cf1d74db568c065721d3a31a9a13a7609d
Nastia Kartseva <danetzachem@gmail.com>
2022-08-04 20:06:33 +0300

    organise app file structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,127 +1,16 @@
 import { createStore } from "vuex";
 import { uniqueId } from "lodash";
-import formatPrice from "./formatPrice.js";
 import axios from "axios";
+
+import formatPrice from "../formatPrice.js";
+import { selectedItems, watchedItems } from "./storeDataFixtures.js";
 
 const store = createStore({
   state: {
     cart: {
-      selectedItems: [
-        {
-          id: 1,
-          image: "src/assets/SelectedItem1.png",
-          name: "Вытяжное устройство G2H",
-          description:
-            "12-72/168 м3/ч / гидрорегулируемый расход /  от датчика присутствия",
-          number: "G2H1065",
-          price: "12 644",
-          count: 1,
-        },
-        {
-          id: 2,
-          image: "src/assets/SelectedItem2.png",
-          name: "Вытяжное устройство BXC",
-          description:
-            "12-72/168 м3/ч / гидрорегулируемый расход /  от датчика присутствия",
-          number: "G2H1065",
-          price: "12 644",
-          count: 2,
-        },
-        {
-          id: 3,
-          image: "src/assets/SelectedItem3.png",
-          name: "Вытяжное устройство GHN",
-          description:
-            "12-72/168 м3/ч / гидрорегулируемый расход /  от датчика присутствия",
-          nubmer: "G2H1065",
-          price: "12 644",
-          count: 1,
-        },
-      ],
+      selectedItems,
       needsSetting: false,
-      watchedItems: [
-        {
-          id: 1,
-          image: "src/assets/WatchedItem1.png",
-          name: "BXC",
-          description:
-            "Вытяжное устройство для механической системы вентиляции",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 2,
-          image: "src/assets/WatchedItem2.png",
-          name: "G2H",
-          description: "Многофункциональное вытяжное устройство для естественной и гибридной вентиляции",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 3,
-          image: "src/assets/WatchedItem3.png",
-          name: "GHN",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 4,
-          image: "src/assets/WatchedItem4.png",
-          name: "TDA",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 5,
-          image: "src/assets/WatchedItem4.png",
-          name: "OOO",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 6,
-          image: "src/assets/WatchedItem4.png",
-          name: "TTT",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 7,
-          image: "src/assets/WatchedItem4.png",
-          name: "LLL",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 8,
-          image: "src/assets/WatchedItem4.png",
-          name: "CCC",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 9,
-          image: "src/assets/WatchedItem4.png",
-          name: "BBB",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-        {
-          id: 10,
-          image: "src/assets/WatchedItem4.png",
-          name: "AAA",
-          description: "Вытяжное устройство с датчиком присутствия",
-          rubRange: "6 848 ₽ – 56 584 ₽",
-          eurRange: "77.60 € – 643.86 €",
-        },
-      ],
+      watchedItems,
       checkoutStatus: "initial",
     },
   },
